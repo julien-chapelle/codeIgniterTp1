acec6a9244ffe42eabf817cdd9f2817daaf570a5
Chapelle Julien <you@example.comLHP3.cjulien@gmail.com>
2020-05-26 09:55:26 +0200

    page d'accueil ok, reste a activer le style

--- codeIgniter/application/controllers/Accueil.php
+++ codeIgniter/application/controllers/Accueil.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Site extends CI_Controller {
+class Accueil extends CI_Controller {
     public function index() {
-        $data["title"] = "Page d'accueil";
+        $data['title'] = 'Page d\'accueil';
 
         $this->load->view('common/header', $data);
-        $this ->load->view ('Accueil/index');
+        $this ->load->view('accueil/index', $data);
         $this->load->view('common/footer', $data);
     }
 }
